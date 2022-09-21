f5e9a4c2f1ad43eef535cdf32ff30f9a6a9d3fe9
Luis Jose Padilla Benitez <lupadilla@bansi.com.mx>
2022-09-21 16:55:32 -0500

    Java Swing #1

---
+++ Swing/FlashCardBuilder.java
@@ -0,0 +1,20 @@
+package Swing;
+
+import java.util.ArrayList;
+
+import javax.swing.JTextArea;
+
+public class FlashCardBuilder {
+
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList<FlashCard> cardList;
+
+    public FlashCardBuilder() {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
--- Swing/SwingDemo.java
+++ Swing/SwingDemo.java
@@ -9,9 +9,10 @@ import java.awt.event.ActionListener;
 /**
  * SwingDemo
  */
-public class SwingDemo {
+public class SwingDemo implements ActionListener {
 
     JLabel jLabel;
+    JTextField jTextField;
 
     public SwingDemo() {
 
@@ -28,34 +29,52 @@ public class SwingDemo {
         JButton jButtonUp = new JButton("Up");
         JButton jButtonDown = new JButton("Down");
         JButton jButtonTest = new JButton("Test");
+        JButton jButtonReverse = new JButton("Reverse");
 
         // Add EventListeners
-        jButtonUp.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jLabel.setText("Hello Up Clicked");
+        // jButtonUp.addActionListener(new ActionListener() {
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // jLabel.setText("Hello Up Clicked");
 
-            }
-        });
+        // }
+        // });
 
-        jButtonDown.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jLabel.setText("Hello Down Clicked");
-            }
-        });
+        // jButtonDown.addActionListener(new ActionListener() {
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // jLabel.setText("Hello Down Clicked");
+        // }
+        // });
 
-        jButtonTest.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jLabel.setText("Hello Test Clicked");
-            }
-        });
+        // jButtonTest.addActionListener(new ActionListener(){
+        // @Override
+        // public void actionPerformed(ActionEvent e) {
+        // jLabel.setText("Hello Test Clicked");
+        // }
+        // });
+
+        jButtonUp.addActionListener(this);
+        jButtonDown.addActionListener(this);
+        jButtonTest.addActionListener(this);
+        jButtonReverse.addActionListener(this);
 
         // Add to the frame
         jFrame.add(jButtonUp);
         jFrame.add(jButtonDown);
         jFrame.add(jButtonTest);
+        jFrame.add(jButtonReverse);
+
+        // Create TextField
+        jTextField = new JTextField(10);
+
+        jTextField.setActionCommand("textField");
+
+        // Add EventListener
+        jTextField.addActionListener(this);
+
+        // Add to the frame
+        jFrame.add(jTextField);
 
         // Create a jlabel
         jLabel = new JLabel("Wow this is amazing!");
@@ -68,6 +87,21 @@ public class SwingDemo {
 
         // Show it
         jFrame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Reverse")) {
+            String originalString = jTextField.getText();
+            String reverseString = "";
+            for(int index =  originalString.length() - 1; index >= 0; index --){
+                reverseString += originalString.charAt(index);
+            }
+            jLabel.setText(reverseString);
+        } else {
+
+        }
+        
     }
 
     public static void main(String[] args) {
